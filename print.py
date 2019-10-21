d4d6a2f33a005728a3485dba4f5385ecd93232f1
Ling Li <eclingdi@hotmail.com>
2019-10-21 14:29:20 +0200

    to print letter per letter using for loop

--- print.py
+++ print.py
@@ -1,4 +1,16 @@
-String= “Hello world”
-Print(String)
-for i in String:
+string = "Hello world"
+print(string)
+for i in string:
     print(i)
+
+#to print per 2 character
+for i, s in enumerate(string):
+    if i % 2 == 0:
+        print(string[i-1:i+1])
+
+#another way only to print even numbers
+for i, s in enumerate(string):
+    if i % 2 == 0:
+        print(s, end="")
+print()
+
