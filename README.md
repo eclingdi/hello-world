c0059b66fe786cdcb08681d784e835b69241374c
Ling Li <eclingdi@hotmail.com>
2019-10-21 14:55:16 +0200

    to delete the last part
    to delete the last part

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello-world
 print hello world in different way
+
+to print per 2 or 3 characters
--- print.py
+++ print.py
@@ -8,9 +8,3 @@ for i, s in enumerate(string):
     if i % 2 == 0:
         print(string[i-1:i+1])
 
-#another way only to print even numbers
-for i, s in enumerate(string):
-    if i % 2 == 0:
-        print(s, end="")
-print()
-
